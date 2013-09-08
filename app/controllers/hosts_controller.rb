35247f96a425eea87928b5e5c748775b57d3a7f2
eguitarz <dalema22@gmail.com>
2013-09-08 20:38:24 +0800

    refactor ping

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -12,13 +12,19 @@ class HostsController < ApplicationController
 
 	def ping
 		@host = Host.find(params[:host_id])
+		response = ping_host(@host)
+		render :json => response
+	end
+
+	private 
+	def ping_host(host)
 		start_at = Time.now
-		response = Net::HTTP.get_response( URI(@host.url) )
+		response = Net::HTTP.get_response( URI(host.url) )
 		duration = ( Time.now - start_at ) * 1000
 
-		ping = @host.ping_records.create( :response_ms => duration.floor, :response_code => response.code)
+		ping = host.ping_records.create( :response_ms => duration.floor, :response_code => response.code)
 		ping.save!
 
-		render :json => {'response_ms'=>duration.floor, 'response_code'=>response.code}
+		{'response_ms'=>duration.floor, 'response_code'=>response.code}
 	end
 end
